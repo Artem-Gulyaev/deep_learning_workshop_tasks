0172eb1713b5ff7d8fa98ba93c7285d2b88b0e77
Oleksandr Serkin <OSerkin@luxoft.com>
2018-06-06 17:56:05 +0200

    Initial commit to check repository connectivity

---
+++ workshop2/oserkin/neuronTest.py
@@ -0,0 +1,25 @@
+import numpy as np
+import math
+#import matplotlib.pyplot as plt
+
+
+
+
+
+
+
+
+
+def createDataSet():
+    x = np.random.random(10)
+    np.set_printoptions(precision=0)
+    print(x)
+
+
+
+def main():
+    print("Neron recognition start point")
+    createDataSet();
+
+
+main();
