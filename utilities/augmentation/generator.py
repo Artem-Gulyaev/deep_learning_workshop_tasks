2bbf332b3e5a68f388449ad549755da97b6a7b42
Alexandr Pryadka <APryadka@luxoft.com>
2018-06-08 16:35:58 +0200

    [AUGMENTATION] Switch to one transformation matrix

--- utilities/augmentation/generator.py
+++ utilities/augmentation/generator.py
@@ -3,6 +3,7 @@
 from __future__ import division
 
 import numpy as np
+from numpy.linalg import inv
 import imageio
 import glob
 import json
@@ -12,20 +13,24 @@ def init(configPath):
     with open(configPath) as jsonData:
         data = json.load(jsonData)
         try:
-            print('Accepted following configuration: scale({0}...{1}), rotation({2}...{3}), input:{4}, output:{5}, clones: {6},out size: {7}x{8}'
-            .format(data['scale']['min'],data['scale']['max'],data['rotation']['from'],data['rotation']['to'],
-            data['input_path'],data['output_path'],data['clones_amount'],data['output_shape']['height'],data['output_shape']['width']))
+            print('Accepted following configuration: scale({0}...{1}), '
+            'rotation({2}...{3}), input:{4}, output:{5}, clones: {6},'
+            'out size: {7}x{8}'
+            .format(data['scale']['min'],data['scale']['max'],
+            data['rotation']['from'],data['rotation']['to'],
+            data['input_path'],data['output_path'],data['clones_amount'],
+            data['output_shape']['height'],data['output_shape']['width']))
         except KeyError as err:
             print('Wrong or missing parameter in config file:{0}'.format(err))
             return json.loads('{}')
         return data
 
-def readPNGImage(imagePath):
-    image = imageio.imread(imagePath)
+def readImage(imagePath, format):
+    image = imageio.imread(imagePath, format)
     l = len(image.shape)
-    outImage = np.zeros((image.shape[0],image.shape[1]))
-    for y in range(0,image.shape[0]):
-        for x in range(0,image.shape[1]):
+    outImage = np.zeros((image.shape[0], image.shape[1]))
+    for y in range(0, image.shape[0]):
+        for x in range(0, image.shape[1]):
             if l > 2:
                 c = np.sum(image[y,x,0:3]) / 3.0
             else:
@@ -33,12 +38,6 @@ def readPNGImage(imagePath):
             if c > 128:
                 outImage[y,x] = 255
     return outImage
-
-def readJPEGImage(imagePath):
-    return np.empty((0,0))
-
-def readBMPImage(imagePath):
-    return np.empty((0,0))
 
 def writeImage(imagePath, image):
     #raw_input("Press Enter to continue...")
@@ -56,26 +55,33 @@ def checkPathAndCreatIfNotExist(path):
 def processImage(image, config, fileName):
     counter = 0
     for i in range(0,config['clones_amount']):
-        clone = np.empty((config['output_shape']['height'],config['output_shape']['width']))
+        clone = np.empty((config['output_shape']['height'],
+        config['output_shape']['width']))
         clone.fill(255)
-        alpha = np.random.uniform(config['rotation']['from'],config['rotation']['to'])
+        alpha = np.random.uniform(config['rotation']['from'],
+        config['rotation']['to'])
+
         alpha = alpha / 180.
         c,s = np.cos(alpha), np.sin(alpha)
-        R_matrix = np.array(((c,-s),(s,c)))
+        rotationMatrix = np.array([[c,-s,0],[s,c,0],[0,0,1]])
+
         scale = np.random.uniform(config['scale']['min'], config['scale']['max'])
+        scaleMatrix = np.array([[scale,0,0],[0,scale,0],[0,0,1]])
+
         shiftXMax = clone.shape[1] * (1.0 - scale)
         shiftYMax = clone.shape[0] * (1.0 - scale)
         shift_x = np.random.uniform(-shiftXMax, shiftXMax)
         shift_y = np.random.uniform(-shiftYMax, shiftYMax)
-        S_matrix = np.array([shift_y/clone.shape[0],shift_x/clone.shape[1]])
+        shiftMatrix = np.array([[1,0,shift_y/clone.shape[0]],[0,1,shift_x/clone.shape[1]],[0,0,1]])
+
+        transformationMatrix = inv(scaleMatrix.dot(shiftMatrix).dot(rotationMatrix))
+        print transformationMatrix
         for y in range(0, clone.shape[0]):
             for x in range(0, clone.shape[1]):
-                sourceCoordinate = np.array([y/clone.shape[0] - 0.5, x/clone.shape[1] - 0.5])
-                sourceCoordinate = sourceCoordinate / scale
+                destinationCoordinate = np.array([y/clone.shape[0] - 0.5, x/clone.shape[1] - 0.5, 1])
 
-                sourceCoordinate = sourceCoordinate + S_matrix
-                sourceCoordinate = R_matrix.dot(sourceCoordinate.T)
-                if (sourceCoordinate < 0.5).all() and (sourceCoordinate > -0.5).all():
+                sourceCoordinate = transformationMatrix.dot(destinationCoordinate)
+                if (sourceCoordinate[0:2] < 0.5).all() and (sourceCoordinate[0:2] > -0.5).all():
                     s_y = int((sourceCoordinate[0] + 0.5) * image.shape[0])
                     s_x = int((sourceCoordinate[1] + 0.5) * image.shape[1])
                     clone[y,x] = image[s_y,s_x]
@@ -94,13 +100,13 @@ def main():
             fileName,extention = fileNameWithExtention.split('.')
             if extention == 'png':
                 print('--PNG: {0}'.format(fileName))
-                image = readPNGImage(imagePath)
+                image = readImage(imagePath, 'PNG-PIL')
             elif extention == 'png':
                 print('--JPEG: {0}'.format(fileName))
-                image = readJPEGImage(imagePath)
+                image = readImage(imagePath, 'JPEG-PIL')
             elif extention == 'bmp':
                 print('--BMP: {0}'.format(fileName))
-                image = readBMPImage(imagePath)
+                image = readImage(imagePath, 'BMP-PIL')
             else:
                 continue
 
