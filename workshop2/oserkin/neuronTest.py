3c4e28d10c0f0fad6dde19462be2bd93a9dbda6d
Oleksandr Serkin <OSerkin@luxoft.com>
2018-06-08 16:34:49 +0200

    save current draft state

--- workshop2/oserkin/neuronTest.py
+++ workshop2/oserkin/neuronTest.py
@@ -2,24 +2,31 @@ import numpy as np
 import math
 #import matplotlib.pyplot as plt
 
+def createDataSet(img_w,img_h,m):
+    nx = img_w * img_h
+    X = np.zeros((m,nx))
+    Y = np.zeros((1,m))
+    print ("X = ",X)
+    print ("Y = ",Y)
+    return (X,Y)
 
-
-
-
-
-
-
-
-def createDataSet():
-    x = np.random.random(10)
-    np.set_printoptions(precision=0)
-    print(x)
-
+def initFun(params,nx):
+    W = np.random.randn(1,nx) ##/ nx
+    B = np.zeros((1,1))
+    params["W"] = W # weight vector
+    params["B"] = B
+    return (W,B)
 
 
 def main():
-    print("Neron recognition start point")
-    createDataSet();
+    img_w = 2
+    img_h = 2
+    trainigSamplesNum = 5
+    print("Neron   recognition start point")
+    createDataSet(img_w,img_h,trainigSamplesNum)
+    params = {}
+    initFun(params,img_w*img_h)
+    print(params)
 
 
-main();
+main()
